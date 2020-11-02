c7943f1fda84849d3af04f2fd442aa54a9657bfb
siubie <putraprima@gmail.com>
2020-11-02 07:16:50 +0700

    01-LiveData

--- app/src/main/java/id/putraprima/viewmodelpushupcouter/viewmodels/MainActivityViewModel.java
+++ app/src/main/java/id/putraprima/viewmodelpushupcouter/viewmodels/MainActivityViewModel.java
@@ -2,23 +2,22 @@ package id.putraprima.viewmodelpushupcouter.viewmodels;
 
 import android.util.Log;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import id.putraprima.viewmodelpushupcouter.models.PushupCount;
 
 public class MainActivityViewModel extends ViewModel {
+    public MutableLiveData<PushupCount> pushupCountMutableLiveData = new MutableLiveData<>();
     public PushupCount pushupCount;
 
     public MainActivityViewModel(PushupCount pushupCount) {
         this.pushupCount = pushupCount;
+        this.pushupCountMutableLiveData.setValue(pushupCount);
     }
 
     public void doPushup(){
         this.pushupCount.setCount(this.pushupCount.getCount()+1);
-        Log.d("Pushup Count", String.valueOf(this.getPushup()));
-    }
-
-    public int getPushup(){
-       return this.pushupCount.getCount();
+        this.pushupCountMutableLiveData.setValue(pushupCount);
     }
 }
